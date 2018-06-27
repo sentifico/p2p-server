e0a74aae208bee134751133b1bad36e775370bd4
sentifico <40645863+sentifico@users.noreply.github.com>
2018-06-28 01:14:46 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
-m={}
 require("socket.io").listen(process.env.PORT||3000).on("connection",(socket)=>{
   console.log("Peer connected.")
   socket.on("msg",(e)=>{
-    m[Math.random()]=e.data
+    memory=e.data
   })
   setInterval(()=>{
-    for(i in m){
-      socket.emit("msg",{data:m[i]})
-    }
+    socket.emit("msg",{data:memory})
   },1)
 })
